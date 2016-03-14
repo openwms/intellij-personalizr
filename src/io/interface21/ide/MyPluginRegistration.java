fa0423255fd36cb167a37b312acdc6a722835b0f
Heiko Scherrer <scherrer@openwms.org>
2016-03-14 23:08:42 +0100

    Initial version

---
+++ src/io/interface21/ide/MyPluginRegistration.java
@@ -0,0 +1,58 @@
+/*
+ * Stamplets.
+ *
+ * This software module including the design and software principals used is and remains
+ * the property of Heiko Scherrer (the initial authors of the project) with the understanding
+ * that it is not to be reproduced nor copied in whole or in part, nor licensed or otherwise
+ * provided or communicated to any third party without their prior written consent.
+ * It must not be used in any way detrimental to the interests of the author.
+ * Acceptance of this module will be construed as an agreement to the above. 
+ *
+ * All rights of Heiko Scherrer remain reserved.
+ * Stamplets is an registered trademarks of Heiko Scherrer. Other products and company
+ * names mentioned herein may be trademarks or trade names of the respective owner.
+ * Specifications are subject to change without notice.
+ */
+package io.interface21.ide;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.components.ApplicationComponent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A MyPluginRegistration.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version 1.0
+ * @since 1.0
+ */
+public class MyPluginRegistration implements ApplicationComponent {
+    // Returns the component name (any unique string value).
+    @NotNull
+    public String getComponentName() {
+        return "MyPlugin";
+    }
+
+
+    // If you register the MyPluginRegistration class in the <application-components> section of
+    // the plugin.xml file, this method is called on IDEA start-up.
+    public void initComponent() {
+        ActionManager am = ActionManager.getInstance();
+        TextBoxes action = new TextBoxes();
+
+        // Passes an instance of your custom TextBoxes class to the registerAction method of the ActionManager class.
+        am.registerAction("MyPluginAction", action);
+
+        // Gets an instance of the WindowMenu action group.
+        DefaultActionGroup windowM = (DefaultActionGroup) am.getAction("WindowMenu");
+
+        // Adds a separator and a new menu command to the WindowMenu group on the main menu.
+        windowM.addSeparator();
+        windowM.add(action);
+    }
+
+    // Disposes system resources.
+    public void disposeComponent() {
+    }
+}
---
+++ src/io/interface21/ide/TextBoxes.java
@@ -0,0 +1,47 @@
+/*
+ * Stamplets.
+ *
+ * This software module including the design and software principals used is and remains
+ * the property of Heiko Scherrer (the initial authors of the project) with the understanding
+ * that it is not to be reproduced nor copied in whole or in part, nor licensed or otherwise
+ * provided or communicated to any third party without their prior written consent.
+ * It must not be used in any way detrimental to the interests of the author.
+ * Acceptance of this module will be construed as an agreement to the above. 
+ *
+ * All rights of Heiko Scherrer remain reserved.
+ * Stamplets is an registered trademarks of Heiko Scherrer. Other products and company
+ * names mentioned herein may be trademarks or trade names of the respective owner.
+ * Specifications are subject to change without notice.
+ */
+package io.interface21.ide;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+/**
+ * A TextBoxes.
+ *
+ * @author <a href="mailto:scherrer@openwms.org">Heiko Scherrer</a>
+ * @version 1.0
+ * @since 1.0
+ */
+public class TextBoxes extends AnAction {
+    // If you register the action from Java code, this constructor is used to set the menu item name
+    // (optionally, you can specify the menu description and an icon to display next to the menu item).
+    // You can omit this constructor when registering the action in the plugin.xml file.
+    public TextBoxes() {
+        // Set the menu item name.
+        super("Text _Boxes");
+        // Set the menu item name, description and icon.
+        // super("Text _Boxes","Item description",IconLoader.getIcon("/Mypackage/icon.png"));
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+    }
+}
